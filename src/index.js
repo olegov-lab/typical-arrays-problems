323fa5ec463355c2dc190f60602138409fe133f9
olegov-lab <olego8760@gmail.com>
2021-01-30 13:30:11 +0300

    test: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 
 exports.min = function min (array) {
-  return 0;
+
+    if (!Array.isArray(array) || array.length == 0 || array == undefined) {
+        return 0;
+    }
+
+    let small = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+
+        if (array[i] < small) {
+           small = array[i];
+        }
+    }
+    return small;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+    if (!Array.isArray(array) || array.length == 0 || array == undefined) {
+        return 0;
+    }
+
+    let max = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+
+        if (array[i] > max) {
+           max = array[i];
+        }
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+    if (!Array.isArray(array) || array.length == 0 || array == undefined) {
+        return 0;
+    }
+
+    let sum = 0;
+    let avg = 0;
+
+   for (let i = 0; i < array.length; i++) {
+
+       sum += array[i];
+   }
+
+    avg = sum/+array.length;
+    return avg;
+
 }
+
